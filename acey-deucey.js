f6d5d986c76bbc533f6dcb8f3758cb0c04c23203
Goshman <sarah.goshman@gmail.com>
2017-04-11 18:44:11 -0400

    added in the other half of the deck

--- acey-deucey.js
+++ acey-deucey.js
@@ -23,9 +23,36 @@ deck = ['https://upload.wikimedia.org/wikipedia/commons/3/36/Playing_card_club_A
 'https://upload.wikimedia.org/wikipedia/commons/3/34/Playing_card_diamond_10.svg', 
 'https://upload.wikimedia.org/wikipedia/commons/a/af/Playing_card_diamond_J.svg', 
 'https://upload.wikimedia.org/wikipedia/commons/0/0b/Playing_card_diamond_Q.svg', 
-'https://upload.wikimedia.org/wikipedia/commons/7/78/Playing_card_diamond_K.svg']
+'https://upload.wikimedia.org/wikipedia/commons/7/78/Playing_card_diamond_K.svg',
+'https://upload.wikimedia.org/wikipedia/commons/2/25/Playing_card_spade_A.svg', 
+'https://upload.wikimedia.org/wikipedia/commons/f/f2/Playing_card_spade_2.svg',
+'https://upload.wikimedia.org/wikipedia/commons/5/52/Playing_card_spade_3.svg',
+'https://upload.wikimedia.org/wikipedia/commons/2/2c/Playing_card_spade_4.svg',
+'https://upload.wikimedia.org/wikipedia/commons/9/94/Playing_card_spade_5.svg',
+'https://upload.wikimedia.org/wikipedia/commons/d/d2/Playing_card_spade_6.svg',
+'https://upload.wikimedia.org/wikipedia/commons/6/66/Playing_card_spade_7.svg',
+'https://upload.wikimedia.org/wikipedia/commons/2/21/Playing_card_spade_8.svg', 
+'https://upload.wikimedia.org/wikipedia/commons/e/e0/Playing_card_spade_9.svg',
+'https://upload.wikimedia.org/wikipedia/commons/8/87/Playing_card_spade_10.svg',
+'https://upload.wikimedia.org/wikipedia/commons/b/bd/Playing_card_spade_J.svg',
+'https://upload.wikimedia.org/wikipedia/commons/5/51/Playing_card_spade_Q.svg', 
+'https://upload.wikimedia.org/wikipedia/commons/9/9f/Playing_card_spade_K.svg', 
+'https://upload.wikimedia.org/wikipedia/commons/5/57/Playing_card_heart_A.svg',
+'https://upload.wikimedia.org/wikipedia/commons/d/d5/Playing_card_heart_2.svg', 
+'https://upload.wikimedia.org/wikipedia/commons/b/b6/Playing_card_heart_3.svg', 
+'https://upload.wikimedia.org/wikipedia/commons/a/a2/Playing_card_heart_4.svg', 
+'https://upload.wikimedia.org/wikipedia/commons/5/52/Playing_card_heart_5.svg', 
+'https://upload.wikimedia.org/wikipedia/commons/c/cd/Playing_card_heart_6.svg', 
+'https://upload.wikimedia.org/wikipedia/commons/9/94/Playing_card_heart_7.svg',
+'https://upload.wikimedia.org/wikipedia/commons/5/50/Playing_card_heart_8.svg',
+'https://upload.wikimedia.org/wikipedia/commons/5/50/Playing_card_heart_9.svg',
+'https://upload.wikimedia.org/wikipedia/commons/9/98/Playing_card_heart_10.svg',
+'https://upload.wikimedia.org/wikipedia/commons/4/46/Playing_card_heart_J.svg',
+'https://upload.wikimedia.org/wikipedia/commons/7/72/Playing_card_heart_Q.svg',
+'https://upload.wikimedia.org/wikipedia/commons/d/dc/Playing_card_heart_K.svg',
+]
 
-deckvals = [1,2,3,4,5,6,7,8,9,10,11,12,13,1,2,3,4,5,6,7,8,9,10,11,12,13]
+deckvals = [1,2,3,4,5,6,7,8,9,10,11,12,13,1,2,3,4,5,6,7,8,9,10,11,12,13,1,2,3,4,5,6,7,8,9,10,11,12,13,1,2,3,4,5,6,7,8,9,10,11,12,13]
 
 function drawCard() 
 	{
